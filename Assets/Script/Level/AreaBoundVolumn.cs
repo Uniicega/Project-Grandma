5ebc64a653f6f007077bd4192f2f91f8d2b9e673
Longboye99 <144575154+Longboye99@users.noreply.github.com>
2025-10-10 06:06:16 +0700

    create level area

--- Assets/Script/Level/AreaBoundVolumn.cs
+++ Assets/Script/Level/AreaBoundVolumn.cs
@@ -6,8 +6,9 @@ public class AreaBoundVolumn : MonoBehaviour
     public AreaEnum area;
     private void OnTriggerEnter(Collider other)
     {
-        if(other.gameObject.tag == "Player")
+        if(other.gameObject.tag == "PlayerCollider")
         {
+            Debug.Log(other.gameObject.name + " Entered Area : " + area.ToString());
             GameManager.instance.anomalyManager.currentArea = area;
         }
     }
